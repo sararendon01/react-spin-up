bce2421e3ed9cb19ff7d7c47afee63552b668e71
srendon <srendon4@cnm.edu>
2020-05-11 13:45:12 -0600

    created

---
+++ app/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.css';
+import {BrowserRouter} from "react-router-dom";
+import {Route, Switch} from "react-router";
+import {FourOhFour} from "./pages/FourOhFour";
+import {Home} from "./pages/Home";
+import {Other} from "./pages/Other";
+
+
+const Routing = () => (
+	<>
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/" component={Home}/>
+				<Route exact path="/Other" component={Other}/>
+				<Route component={FourOhFour}/>
+			</Switch>
+		</BrowserRouter>
+	</>
+);
+ReactDOM.render(<Routing/>, document.querySelector('#root'));
---
+++ app/src/pages/Other.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const Other = () => {
+	return (
+		<>
+			<h1>Other</h1>
+		</>
+	)
+};
